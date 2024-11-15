cd973a88bc0fe2de527e5b1162d3320ad89d3e7d
ryuux05 <111038563+ryuux05@users.noreply.github.com>
2024-11-16 04:00:11 +0900

    deposit and withdraw function

--- contracts/konektion.sol
+++ contracts/konektion.sol
@@ -31,8 +31,8 @@ contract Konektion is ReentrancyGuard , EIP712("Konektion", "1")  {
     );
 
     //Events
-    event Deposit(address indexed address, uint256 amount);
-    event Withdraw(address indexed address, uint256 amount);
+    event Deposit(address indexed address, uint256 amount, uint256 balance);
+    event Withdraw(address indexed address, uint256 amount, uint256 balance);
     event Payment(address indexed from, address indexed to, uint256 amount);
     event SignatureVerified();
 
@@ -43,19 +43,36 @@ contract Konektion is ReentrancyGuard , EIP712("Konektion", "1")  {
                 keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"),
                 keccak256(bytes("SimplyPay")),        // name
                 keccak256(bytes("1.0")),            // version
-                1,                      // chainId
+                block.chainId,                      // chainId
                 address(this)                       // verifyingContract
             )
         );
 
     }
 
-    function Deposit(uint256 amount) nonReentrant external {
+    function Deposit(uint256 amount) nonReentrant external payable{
+        require(msg.value >= _tokenAmount, "Insufficient ether.");
+        require(msg.value > 0, "You can't deposit 0 ether");
 
+        //Update balance mapping
+        balances[msg.sender] += msg.value;
+
+        //Emit event
+        emit Deposit(msg.sender, msg.value, balances[msg.sender]);
     }
 
-    function Withdraw() {
+    function Withdraw(uint256 amount) {
+        require(msg.value <= balances[msg.sender], "Insufficient funds");
+        require(msg.value > 0, "You can't withdraw 0 ether");
 
+        //Update balance mapping
+        balances[msg.sender] -= amount;
+
+        //Release the fund
+        payable(msg.sender).transfer(amount);
+
+        //Emit event
+        emit Withdraw(msg.sender, amount, balances[msg.sender])
     }
 
     function Verify() {
